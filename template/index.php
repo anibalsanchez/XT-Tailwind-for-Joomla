b7cd06e31e9539b0caf2198d798299e932e946aa
anibalsanchez <anibal.sanchez@extly.com>
2021-06-09 17:31:08 +0200

    Publish prefixed build 2021-06-09 17:31:08

--- template/index.php
+++ template/index.php
@@ -1,4 +1,5 @@
 <?php
+/* This file has been prefixed by <PHP-Prefixer> for "XT Tailwind CSS" */
 
 /*
  * @package     XT Tailwind for Joomla
@@ -14,12 +15,12 @@ defined('_JEXEC') or die;
 
 require_once JPATH_ROOT.'/libraries/xttailwind/vendor/autoload.php';
 
-use Extly\Infrastructure\Service\Cms\Joomla\ScriptHelper;
-use Extly\Infrastructure\Support\HtmlAsset\Asset\InlineScriptTag;
-use Extly\Infrastructure\Support\HtmlAsset\Asset\LinkCriticalStylesheetTag;
-use Extly\Infrastructure\Support\HtmlAsset\Asset\LinkStylesheetTag;
-use Extly\Infrastructure\Support\HtmlAsset\Asset\ScriptTag;
-use Extly\Infrastructure\Support\HtmlAsset\Repository as HtmlAssetRepository;
+use XTP_BUILD\Extly\Infrastructure\Service\Cms\Joomla\ScriptHelper;
+use XTP_BUILD\Extly\Infrastructure\Support\HtmlAsset\Asset\InlineScriptTag;
+use XTP_BUILD\Extly\Infrastructure\Support\HtmlAsset\Asset\LinkCriticalStylesheetTag;
+use XTP_BUILD\Extly\Infrastructure\Support\HtmlAsset\Asset\LinkStylesheetTag;
+use XTP_BUILD\Extly\Infrastructure\Support\HtmlAsset\Asset\ScriptTag;
+use XTP_BUILD\Extly\Infrastructure\Support\HtmlAsset\Repository as HtmlAssetRepository;
 use Joomla\CMS\Factory as CMSFactory;
 use Joomla\CMS\HTML\HTMLHelper as CMSHTMLHelper;
 use Joomla\CMS\Uri\Uri as CMSUri;
