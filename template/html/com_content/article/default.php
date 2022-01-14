9844304b3fbac2fdec69f0e2c9f36176df969bcd
anibalsanchez <anibal.sanchez@extly.com>
2022-01-14 13:06:24 +0100

    style(theme): Tweak final

---
+++ template/html/com_content/article/default.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * @package     XT Tailwind for Joomla
+ *
+ * @author      Extly, CB. <team@extly.com>
+ * @copyright   Copyright (c)2012-2022 Extly, CB. All rights reserved.
+ * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
+ *
+ * @see         https://www.extly.com
+ */
+
+defined('_JEXEC') || exit;
+
+ob_start();
+require JPATH_ROOT.'/components/com_content/tmpl/article/default.php';
+$item = ob_get_contents();
+ob_end_clean();
+
+echo str_replace('class="com-content-article__body"', 'class="prose com-content-article__body"', $item);
